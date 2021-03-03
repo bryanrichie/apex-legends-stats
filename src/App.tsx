61d0c349ee4d52f8665e585e2441c6dfa0d1949c
Bryan Lee <bryanrichie97@gmail.com>
2021-03-03 15:37:52 +1100

    feat(app): created the skeleton for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SearchIcon } from '@chakra-ui/icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex flexDirection="column" minHeight="100vh">
+      <Box
+        bgImage="url('./kings-canyon-bg.jpg')"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        filter="blur(3px)"
+        zIndex={-1}
+      />
+      <Flex bg="gray.700" w="100%" align="center" borderBottomLeftRadius={50}>
+        <Image src="./apex-legends-logo.png" alt="logo" boxSize="100px" ml={5} mt={5} mb={5} />
+        <Flex flexDir="column" align="center" mr={70}>
+          <Heading color="white">Apex Legends</Heading>
+          <Heading color="white">Stats</Heading>
+        </Flex>
+        <InputGroup w={300}>
+          <Input placeholder="Enter Origin Username" bg="white" />
+          <InputRightElement
+            children={<IconButton aria-label="Search API" icon={<SearchIcon />} />}
+          />
+        </InputGroup>
+
+        <HStack>
+          <Text color="white" fontSize={20} fontWeight="bold" ml={50}>
+            Leaderboards
+          </Text>
+        </HStack>
+      </Flex>
+    </Flex>
   );
 }
 
